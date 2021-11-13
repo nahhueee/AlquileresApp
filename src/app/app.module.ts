f5815a86f2308b75a01b189a37696ec68b52aeef
nahhueee <nahu852na@gmail.com>
2021-11-12 21:45:14 -0300

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'primeng/button';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { InicioComponent } from './componets/inicio/inicio.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
